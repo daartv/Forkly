6ac2edfe472055720ecf6377b25685b3a346194a
Mycah Jay Anast <mycahjay@gmail.com>
2017-03-28 16:41:51 -0700

    Refactored code

--- server/index.js
+++ server/index.js
@@ -1,49 +1,50 @@
 // If Heroku is NOT running, run on local computer and pull local info from setup.js
 if (!process.env.CLIENT_ID) {
   // Note: index.js runs before setup.js (see server-dev script in package.json)
-  var setup = require('./setup.js');
-};
+  const setup = require('./setup')
+}
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var items = require('../db');
-var handler = require('./requestHandler.js');
-var facebook = require('./facebook.js');
-var passport = require('passport');
+const express = require('express')
+const bodyParser = require('body-parser')
+const items = require('../db')
+const handler = require('./requestHandler')
+const facebook = require('./facebook')
+const passport = require('passport')
+const path = require('path')
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
-var app = express();
+const app = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.use(express.static(__dirname + '/../react/dist'));
+app.use(express.static(path.join(__dirname, '/../react/dist')))
 
 // for facebook passport authentication - don't move
-app.use(express.session({ secret: 'rum ham' }));
-app.use(express.cookieParser());
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(express.session({ secret: 'rum ham' }))
+app.use(express.cookieParser())
+app.use(passport.initialize())
+app.use(passport.session())
 
 // for Home Component - from searchRecipes function
-app.post('/searchRecipes', handler.searchRecipes);
+app.post('/searchRecipes', handler.searchRecipes)
 
 // for AddRecipe Component - from handleSubmit function
-app.post('/api/addRecipe', handler.addRecipe);
+app.post('/api/addRecipe', handler.addRecipe)
 
 // for Nav Component - from getUsername function
-app.get('/username', handler.getUsername);
+app.get('/username', handler.getUsername)
 
 // to verify login
 app.get('/verifylogin', (req, res) => {
-  res.send(req.user);
-});
+  res.send(req.user)
+})
 
 // facebook passport
 app.get('/auth/facebook',
   passport.authenticate('facebook')
-);
+)
 
 app.get('/auth/facebook/callback',
   passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' })
@@ -51,21 +52,21 @@ app.get('/auth/facebook/callback',
   //   // Successful authentication, redirect home.
   //   res.redirect('/');
   // });
-);
+)
 
-app.get('/logout', function(req, res){
-  req.logout();
+app.get('/logout', function (req, res) {
+  req.logout()
   // req.session.destroy();
-  res.redirect('/');
-});
+  res.redirect('/')
+})
 
-app.post('/searchRecipes', handler.searchRecipes);
+app.post('/searchRecipes', handler.searchRecipes)
 
 // to find all recipes for given user id
-app.get('/getAllRecipes', handler.getUserRecipes);
+app.get('/getAllRecipes', handler.getUserRecipes)
 
-app.post('/getRecipeById', handler.getRecipeById);
+app.post('/getRecipeById', handler.getRecipeById)
 
-app.listen(port, function() {
-  console.log('listening on port '+ port + '!');
-});
+app.listen(port, function () {
+  console.log('listening on port ' + port + '!')
+})
--- server/requestHandler.js
+++ server/requestHandler.js
@@ -1,71 +1,70 @@
-var express = require('express');
-var request = require('request');
-var mongoose = require('mongoose');
-mongoose.Promise = require('bluebird');
+const express = require('express')
+const request = require('request')
+const mongoose = require('mongoose')
+mongoose.Promise = require('bluebird')
 
-var db = require("../db/index.js");
+const db = require('../db/index')
 
 // for Home Component - from searchRecipes function
-exports.searchRecipes = function(req, res) {
-  var searchTerm = req.body.searchTerm;
- 
+exports.searchRecipes = function (req, res) {
+  const searchTerm = req.body.searchTerm
+
   // regex -> allows the search to contain string instead of === string
   // options i -> allows search to be case insensitive
-  db.Recipe.find({name:{'$regex' : searchTerm, '$options' : 'i'}})
+  db.Recipe.find({name: {'$regex': searchTerm, '$options': 'i'}})
     .exec(function (err, recipe) {
-      if (err) 
-      	{
-      	  return err;
-      	} else {
-      	res.json(recipe);
+      if (err) {
+        return err
+      } else {
+        res.json(recipe)
       }
-	});
-};
+    })
+}
 
 // for Nav Component - from getUsername function
-exports.getUsername = function(req, res) {
+exports.getUsername = function (req, res) {
   if (req.user) {
-    res.json(req.user.name);  
+    res.json(req.user.name)
   } else {
-    res.json(null);
-  }
-};
-
-// for viewRecipes Component - get all recipes for user
-exports.getUserRecipes = function(req, res) {
-  if (req.user) {
-    db.User.findById(req.user._id)
-    .populate('recipes')
-    .exec(function(err, user) {
-      res.send(user.recipes);
-    });
-  } else {
-    res.end();
+    res.json(null)
   }
 }
 
-exports.addRecipe = function(req, res) {
+// for viewRecipes Component - get all recipes for user
+exports.getUserRecipes = function (req, res) {
   if (req.user) {
-    req.body._creator = req.user._id;
+    db.User.findById(req.user._id)
+    .populate('recipes')
+    .exec(function (err, user) {
+      res.send(user.recipes)
+    })
+  } else {
+    res.end()
+  }
+}
+
+exports.addRecipe = function (req, res) {
+  if (req.user) {
+    req.body._creator = req.user._id
 
     // create recipe in database
-    let recipeId;
+    let recipeId
     db.Recipe.create(req.body).then((recipe) => {
       // push recipe into user's recipes array
-      recipeId = recipe.id;
+      recipeId = recipe.id
       db.User.findByIdAndUpdate(req.user._id, {$push: {recipes: recipe.id}})
       .then(() => {
-        res.json(recipeId);
+        res.json(recipeId)
       })
-    });
+    })
   } else {
-    res.end();
+    res.end()
   }
-};
+}
 
-exports.getRecipeById = function(req, res) {
+exports.getRecipeById = function (req, res) {
   db.Recipe.findById(req.body.id)
   .then((recipe) => {
-    res.json(recipe);
+    res.json(recipe)
   })
-};
+}
