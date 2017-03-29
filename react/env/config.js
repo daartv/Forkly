30ffc8e42b4163a0da1f54f7b964850de4f323ee
Eugene Penaranda <eugenepenaranda007@gmail.com>
2017-03-28 23:04:28 -0700

    Update config.js

--- react/env/config.js
+++ react/env/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   'facebookAuth':
   {
-    clientID: '279588619132464',
-    clientSecret: '32e346f0296b752354db3328bd6280d4',
-    callbackURL: 'auth/facebook/callback'
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: process.env.SITE_URL + 'auth/facebook/callback'
   }
 }
