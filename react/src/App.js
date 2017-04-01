032c3d6ea6129a3e002667b6ea18c0f1b3d777cc
Steven Reed <steve.t.reed@gmail.com>
2017-04-01 08:52:34 -0700

    just making sure app is same as this branch.

--- react/src/App.js
+++ react/src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react'
+/**
+ * Utilities
+ */
+import { BrowserRouter as Router, Route, Redirect, Link } from 'react-router-dom'
+import axios from 'axios'
 import $ from 'jquery'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
-// import '../node_modules/react-grid-layout/css/styles.css'
-// import '../node_modules/react-resizable/css/styles.css'
+
+/**
+ * Components
+ */
+import Main from './components/main/Main'
+import MainPageUser from './components/mainPageUser/MainPageUser'
+import MainPageNonUser from './components/mainPageNonUser/MainPageNonUser'
+import SignUpPage from './components/signUpPage/SignUpPage'
+import LoginPage from './components/loginPage/LoginPage'
 
 import Home from './components/Home'
-import LandingPage from './components/landingPage/LandingPage'
 import AddRecipe from './components/AddRecipe'
 import Login from './components/Login'
 import ViewFork from './components/ViewFork'
 import ViewRecipes from './components/ViewRecipes'
 import SearchRecipes from './components/searchRecipes/SearchRecipes'
-import MainPageUser from './components/mainPageUser/MainPageUser'
-import MainPageNonUser from './components/mainPageNonUser/MainPageNonUser'
-import SignUpPage from './components/signUpPage/SignUpPage'
-
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
+/**
+ * Styles
+ */
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+// import '../node_modules/react-grid-layout/css/styles.css'
+// import '../node_modules/react-resizable/css/styles.css'
 
 class App extends Component {
   constructor (props) {
@@ -33,12 +40,11 @@ class App extends Component {
   }
 
   componentDidMount () {
-    this.getUsername()
+    // this.getUsername()
   }
 
   getUsername () {
     var context = this
-
     $.ajax({
       url: '/username',
       type: 'GET',
@@ -67,47 +73,53 @@ class App extends Component {
       }
     })
   }
+
+  /**
+   * getUsername and logout using axios xoxo Mycah
+   */
+
+/*  getUsername () {
+      axios.get('/username')
+      .then(res => {
+        const username = res.data
+        this.setState({ username })
+      })
+      .catch(error => console.log(error))
+    }
+    logout () {
+      axios.get('/logout')
+      .then(res => {
+        console.log('Success! Response is ', res)
+        this.getUsername()
+      })
+      .catch(error => console.log(error))
+    } */
+
+  session () {
+    return false
+    /* * change to true or false to 'mimic' a session * */
+    /* * should return true if session is active else return false * */
+  }
+
+  checkSession () {
+    if (this.session()) {
+      return <Redirect to='/home' />
+    } else {
+      return <Redirect to='/welcome' />
+    }
+  }
+
   render () {
     return (
       <Router>
         <div>
-          {/* <div className='group'>
-            <section className='floatLeft'>
-              <img className='logo' src='assets/images/forkly.png' alt='FORKLY' />
-              <h3 className='title username'><Login username={this.state.username} /></h3>
-            </section>
-            <section className='floatRight'>
-              <nav>
-                <div className='icon logout'>
-                  <img className='navButton' src='assets/images/logout.png' alt='Logout' />
-                  <span><Link to='/' onClick={this.logout}><br />Logout</Link></span>
-                </div>
-                <div className='icon addRecipe'>
-                  <img className='navButton' src='assets/images/addRecipe.png' alt='Add Recipe' />
-                  <span><Link to='/addrecipe'>Create Recipe</Link></span>
-                </div>
-                <div className='icon myForks'>
-                  <img className='navButton' src='assets/images/fork.png' alt='My Recipes' />
-                  <span><Link to='/myrecipes'>My Recipes</Link></span>
-                </div>
-                <div className='icon home'>
-                  <img className='navButton' src='assets/images/home.png' alt='Home' />
-                  <span><Link to='/'><br />Home</Link></span>
-                </div>
-              </nav>
-            </section>
-          </div> */}
-          <Route exact path='/' component={Home} />
-          <Route exact path='/login' component={Login} />
-          <Route path='/user' component={MainPageUser} />
+          <Route exact path='/' render={this.checkSession.bind(this)} />
+          <Route path='/home/' render={this.checkSession.bind(this)} />
+          <Route path='/welcome' component={Main} />
+          <Route path='/home' component={MainPageUser} />
+          <Route path='/forkly' component={MainPageNonUser} />
           <Route path='/signup' component={SignUpPage} />
-          {/* <br />
-          <br />
-          <br />
-          <footer>
-            <br />
-            <small>&copy; <a href='https://github.com/TeamForkly/Forkly'>TeamForkly</a></small>
-          </footer> */}
+          <Route path='/login' component={LoginPage} />
         </div>
       </Router>
     )
@@ -116,16 +128,48 @@ class App extends Component {
 
 export default App
 
+/* * * * * * *
+<div className='group'>
+  <section className='floatLeft'>
+    <img className='logo' src='assets/images/forkly.png' alt='FORKLY' />
+    <h3 className='title username'><Login username={this.state.username} /></h3>
+  </section>
+  <section className='floatRight'>
+    <nav>
+      <div className='icon logout'>
+        <img className='navButton' src='assets/images/logout.png' alt='Logout' />
+        <span><Link to='/' onClick={this.logout}><br />Logout</Link></span>
+      </div>
+      <div className='icon addRecipe'>
+        <img className='navButton' src='assets/images/addRecipe.png' alt='Add Recipe' />
+        <span><Link to='/addrecipe'>Create Recipe</Link></span>
+      </div>
+      <div className='icon myForks'>
+        <img className='navButton' src='assets/images/fork.png' alt='My Recipes' />
+        <span><Link to='/myrecipes'>My Recipes</Link></span>
+      </div>
+      <div className='icon home'>
+        <img className='navButton' src='assets/images/home.png' alt='Home' />
+        <span><Link to='/'><br />Home</Link></span>
+      </div>
+    </nav>
+  </section>
+</div>
+<br />
+<br />
+<br />
+<footer>
+  <br />
+  <small>&copy; <a href='https://github.com/TeamForkly/Forkly'>TeamForkly</a></small>
+</footer>
+* * * * * * */
+
 //           // <Route path='/signup' component={SignUpPage} />
 //           // <Route path='/main' component={MainPageNonUser} />
 
 //           // <Route path='/addrecipe' component={AddRecipe} />
 //           // <Route path='/myrecipes' component={ViewRecipes} />
 //           // <Route path='/recipe' component={ViewFork} />
-
-// // To start, run from terminal the following...
-// // npm run react-dev
-// // npm run server-dev
 
 // // Fred, for good luck.
 // // https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/c/e/b/e/highres_253972926.jpeg
