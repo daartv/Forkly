2d5b67953f25c5cf2fe22613cacdae7b011269d8
Mycah Jay Anast <mycahjay@gmail.com>
2017-04-02 17:04:16 -0700

    files up to date

--- react/src/App.js
+++ react/src/App.js
@@ -67,48 +67,6 @@ class App extends Component {
 
 export default App
 
-/* * * * * * *
-<div className='group'>
-  <section className='floatLeft'>
-    <img className='logo' src='assets/images/forkly.png' alt='FORKLY' />
-    <h3 className='title username'><Login username={this.state.username} /></h3>
-  </section>
-  <section className='floatRight'>
-    <nav>
-      <div className='icon logout'>
-        <img className='navButton' src='assets/images/logout.png' alt='Logout' />
-        <span><Link to='/' onClick={this.logout}><br />Logout</Link></span>
-      </div>
-      <div className='icon addRecipe'>
-        <img className='navButton' src='assets/images/addRecipe.png' alt='Add Recipe' />
-        <span><Link to='/addrecipe'>Create Recipe</Link></span>
-      </div>
-      <div className='icon myForks'>
-        <img className='navButton' src='assets/images/fork.png' alt='My Recipes' />
-        <span><Link to='/myrecipes'>My Recipes</Link></span>
-      </div>
-      <div className='icon home'>
-        <img className='navButton' src='assets/images/home.png' alt='Home' />
-        <span><Link to='/'><br />Home</Link></span>
-      </div>
-    </nav>
-  </section>
-</div>
-<br />
-<br />
-<br />
-<footer>
-  <br />
-  <small>&copy; <a href='https://github.com/TeamForkly/Forkly'>TeamForkly</a></small>
-</footer>
-* * * * * * */
-
-//           // <Route path='/signup' component={SignUpPage} />
-//           // <Route path='/main' component={MainPageNonUser} />
-
-//           // <Route path='/addrecipe' component={AddRecipe} />
-//           // <Route path='/recipe' component={ViewFork} />
-
 // // Fred, for good luck.
 // // https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/c/e/b/e/highres_253972926.jpeg
 // // ,,,,,,,,,,,,,,,,,*,,,,,,,***************,,/(//*((/*/*/(((#(#####((((#%%%%(*,,.,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
---
+++ server/setup.js
@@ -0,0 +1,8 @@
+process.env.CLIENT_ID = '279588619132464'
+process.env.CLIENT_SECRET = '32e346f0296b752354db3328bd6280d4'
+process.env.MONGODB_URI = 'mongodb://localhost/forkly-dev'
+process.env.SITE_URL = 'http://localhost:3000/'
+
+exports.YUMMLY_APP_ID = 'aa394476'
+exports.YUMMLY_APP_KEY = 'cd86d4e0e848d722a9fc99cc024b7aab'
+exports.SPOONACULAR_KEY = 'JG6Si02RobmshiFVjx0CB6Kbaa3Up1Aq6TWjsnc6E5rkfhe3U2'
